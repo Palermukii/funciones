8b3a17551be53b40b10c20199bceece515d58ab4
Palermukii <109637082+Palermukii@users.noreply.github.com>
2023-05-16 00:14:58 -0300

    Update leanza.c

--- leanza.c
+++ leanza.c
@@ -3,33 +3,16 @@
 #include <time.h>
 #include <math.h>
 
-int fx(int x){
-return (9 * x) * (9 * x) + 3x - 12;
-
-}
-
-int main(int argc, charargv[]){
-
-int x = atoi(argv[1]); 
-
-printf("F(x) = (9 * x) * (9 * x) + 3*x - 12 = %d\n", fx(x));
-
-}
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-
-int fx(int n){
-return (n* (n + 1)) / 2 ; 
+int fx(int O){
+return (cos(O)) * (cos(O)) + (sen(O)) * (sen(O)) ; 
 
 }
 
 int main(int argc, char argv[]){
 
-int n = atoi(argv[1]); 
+int O = atoi(argv[1]); 
 
 
-printf("F(n) = (n (n + 1)) / 2 = %d\n", fx(n));
+printf("F(O) = (cos(O)) (cos(O)) + (sen(O)) * (sen(O)) = %d\n", fx(O));
 
 }
