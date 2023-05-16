65a9ed287567104ec1cefc5a708d70ab139a9f2f
Palermukii <109637082+Palermukii@users.noreply.github.com>
2023-05-16 00:10:41 -0300

    Update leanza.c

--- leanza.c
+++ leanza.c
@@ -3,23 +3,33 @@
 #include <time.h>
 #include <math.h>
 
-
-
-
-int fx(){
-
-printf("F(x,y) = x+y = %d\n", suma);
-
+int fx(int x){
+return (9 * x) * (9 * x) + 3x - 12;
 
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, charargv[]){
 
 int x = atoi(argv[1]); 
-int y = atoi(argv[2]);
-int suma = x + y
+
+printf("F(x) = (9 * x) * (9 * x) + 3*x - 12 = %d\n", fx(x));
+
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int fx(int n){
+return (n* (n + 1)) / 2 ; 
+
+}
+
+int main(int argc, char argv[]){
+
+int n = atoi(argv[1]); 
 
 
-fx();
-return 0;
+printf("F(n) = (n (n + 1)) / 2 = %d\n", fx(n));
+
 }
